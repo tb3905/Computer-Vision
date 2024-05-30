d8939a0f357d96923b0339ed747cf7faffca83db
TAHA ALI <94356735+tb3905@users.noreply.github.com>
2024-05-31 00:08:03 +0530

    Create example numpy.py

---
+++ example numpy.py
@@ -0,0 +1,20 @@
+import numpy as np
+x=[1,2,3,4,5]
+
+a=type(x)
+print(a)
+
+ar=np.array(x)
+print(type(ar))
+
+b=np.arange(1,15)
+print(b)
+
+c=np.arange(0,19,3)
+print(c)
+
+d=np.zeros((2,4))
+print(d)
+
+e=np.ones((2,3))
+print(e)
