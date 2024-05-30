3c1be528c1d96409a0c025138a2e9f25c4fbff03
TAHA ALI <94356735+tb3905@users.noreply.github.com>
2024-05-31 01:46:18 +0530

    Update image processing_1.py

--- image processing_1.py
+++ image processing_1.py
@@ -11,4 +11,16 @@ print(img.mode)
 img_ar=np.array(img)
 a=img_ar.shape
 print(a)
-print(img_ar)
+#print(img_ar)
+
+print(img.getbbox())
+print(img.size)
+print(img.format)
+print(img.rotate(90))
+
+img1=img.copy()
+print(img.paste(img1,(100,100)))
+print(img.resize(250,250))
+
+crop_img=img.crop(0,0,10,25)
+print(crop_img)
